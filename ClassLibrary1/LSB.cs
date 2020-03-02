2a1767147ba9c88f093c775791245a73511ade0a
Alex Novoselov <sasha.n2001@mail.ru>
2020-03-03 01:26:59 +0300

    добавлена библиотека классов с уже готовыми наработаками

---
+++ ClassLibrary1/LSB.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.IO.Compression;
+
+namespace Encode
+{
+    public class LSB
+    {
+        public void Hide(Stream messageStream, Stream keyStream)
+        {
+
+            byte[] waveBuffer = new byte[bytesPerSample];
+            byte message, bit, waveByte;
+            int messageBuffer; //receives the next byte of the message or -1
+            int keyByte; //distance of the next carrier sample
+
+            //loop over the message, hide each byte
+            while ((messageBuffer = messageStream.ReadByte()) >= 0)
+            {
+                //read one byte of the message stream
+                message = (byte)messageBuffer;
+
+                //for each bit in [message]
+                for (int bitIndex = 0; bitIndex < 8; bitIndex++)
+                {
+
+                    //read a byte from the key
+                    keyByte = GetKeyValue(keyStream);
+
+                    //skip a couple of samples
+                    for (int n = 0; n < keyByte - 1; n++)
+                    {
+                        //copy one sample from the clean stream to the carrier stream
+                        sourceStream.Copy(
+                            waveBuffer, 0,
+                            waveBuffer.Length, destinationStream);
+                    }
+
+                    //read one sample from the wave stream
+                    sourceStream.Read(waveBuffer, 0, waveBuffer.Length);
+                    waveByte = waveBuffer[bytesPerSample - 1];
+
+                    //get the next bit from the current message byte...
+                    bit = (byte)(((message & (byte)(1 << bitIndex)) > 0) ? 1 : 0);
+
+                    //...place it in the last bit of the sample
+                    if ((bit == 1) && ((waveByte % 2) == 0))
+                    {
+                        waveByte += 1;
+                    }
+                    else if ((bit == 0) && ((waveByte % 2) == 1))
+                    {
+                        waveByte -= 1;
+                    }
+
+                    waveBuffer[bytesPerSample - 1] = waveByte;
+
+                    //write the result to destinationStream
+                    destinationStream.Write(waveBuffer, 0, bytesPerSample);
+                }
+            }
+
+            //copy the rest of the wave without changes
+            //...
+        }
+
+        public void Extract(Stream messageStream, Stream keyStream)
+        {
+
+            byte[] waveBuffer = new byte[bytesPerSample];
+            byte message, bit, waveByte;
+            int messageLength = 0; //expected length of the message
+            int keyByte; //distance of the next carrier sample
+
+            while ((messageLength == 0 || messageStream.Length < messageLength))
+            {
+                //clear the message-byte
+                message = 0;
+
+                //for each bit in [message]
+                for (int bitIndex = 0; bitIndex < 8; bitIndex++)
+                {
+
+                    //read a byte from the key
+                    keyByte = GetKeyValue(keyStream);
+
+                    //skip a couple of samples
+                    for (int n = 0; n < keyByte; n++)
+                    {
+                        //read one sample from the wave stream
+                        sourceStream.Read(waveBuffer, 0, waveBuffer.Length);
+                    }
+                    waveByte = waveBuffer[bytesPerSample - 1];
+
+                    //get the last bit of the sample...
+                    bit = (byte)(((waveByte % 2) == 0) ? 0 : 1);
+
+                    //...write it into the message-byte
+                    message += (byte)(bit << bitIndex);
+                }
+
+                //add the re-constructed byte to the message
+                messageStream.WriteByte(message);
+
+                if (messageLength == 0 && messageStream.Length == 4)
+                {
+                    //first 4 bytes contain the message's length
+                    //...
+                }
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Read.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Encode
+{
+
+
+    public class Read
+    {
+        public static void ReadWav(string path)
+        {
+            WavHeader header = new WavHeader();
+            // Размер заголовка
+            int headerSize = Marshal.SizeOf(header);
+
+            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[headerSize];
+            fileStream.Read(buffer, 0, headerSize);
+
+            // Чтобы не считывать каждое значение заголовка по отдельности,
+            // воспользуемся выделением unmanaged блока памяти
+            var headerPtr = Marshal.AllocHGlobal(headerSize);
+            // Копируем считанные байты из файла в выделенный блок памяти
+            Marshal.Copy(buffer, 0, headerPtr, headerSize);
+            // Преобразовываем указатель на блок памяти к нашей структуре
+            Marshal.PtrToStructure(headerPtr, header);
+
+            // Выводим полученные данные
+            Console.WriteLine($"Sample rate: {0}", header.SampleRate);
+            Console.WriteLine($"Channels: {0}", header.NumChannels);
+            Console.WriteLine($"Bits per sample: {0}", header.BitsPerSample);
+
+            // Посчитаем длительность воспроизведения в секундах
+            double durationSeconds = 1.0 * header.Subchunk2Size / (header.BitsPerSample / 8.0) / header.NumChannels / header.SampleRate;
+            int durationMinutes = (int)Math.Floor(durationSeconds / 60);
+            durationSeconds = durationSeconds - (durationMinutes * 60);
+            Console.WriteLine($"Duration: {0:00}:{1:00}", durationMinutes, durationSeconds);
+
+
+            // Освобождаем выделенный блок памяти
+            Marshal.FreeHGlobal(headerPtr);
+        }
+    }
+}
